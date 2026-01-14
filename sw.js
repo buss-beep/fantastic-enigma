d8506590290c01d2df8cd534cf5f8620176b7096
buss-beep <148648823+buss-beep@users.noreply.github.com>
2026-01-14 03:49:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,19 +6,12 @@ try {
 
     const sw = new UVServiceWorker();
 
-    self.addEventListener('install', (event) => {
-        self.skipWaiting(); // Force activation
-    });
-
-    self.addEventListener('activate', (event) => {
-        event.waitUntil(clients.claim()); // Take control of page immediately
-    });
+    self.addEventListener('install', () => self.skipWaiting());
+    self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
 
     self.addEventListener('fetch', (event) => {
         event.respondWith(sw.fetch(event));
     });
-    
-    console.log("SW: Aggressively active on /fantastic-enigma/");
 } catch (e) {
-    console.error("SW: Initialization failed", e);
+    console.error("SW Error:", e);
 }
